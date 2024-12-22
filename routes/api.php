eeb3ce9f08bb9baf39faf4a84519c59ce9632a8e
Ahmed Yahya <a.yahya1@yahoo.com>
2024-12-22 22:21:12 +0200

    feat: Unit Tests

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::prefix('v1')->group(function() {
 
 
     // Authentication Routes (i.e., Getting authenticated)  (Tymon JWT-Auth package authentication)
+    // N.B. This group() can be omitted altogether. It's just mentioned in Tymon JWT-Auth package's documentation
     Route::group([
         'middleware' => 'api', // N.B. This line can be omitted!    // Apply Laravel's built-in 'api' Middleware Group defined in 'vendor\laravel\framework\src\Illuminate\Foundation\Configuration\Middleware.php'. Refer to:    Laravel's Default Middleware Groups: https://laravel.com/docs/11.x/middleware#laravels-default-middleware-groups
         'prefix'     => 'auth' // N.B. This line can be omitted. It's just a prefix mentioned in Tymon JWT-Auth package's documentation
@@ -37,7 +38,7 @@ Route::prefix('v1')->group(function() {
 
 
     // Posts & Comments Routes (Protected Routes which require authentication (Tymon JWT-Auth package)) (i.e., User must be authenticated to access these routes)
-    Route::group(['middleware' => 'auth:api'], function() { // Protect routes (by requiring authentication to access them) that require authentication in our application: Apply Laravel's built-in 'auth' Middleware which requires the user to be authenticated to access the route/s (N.B. 'auth:api' tells Laravel to use the 'api' guard (not 'web') which is configured in 'config/auth.php' file to use the 'jwt' 'driver' of the Tymon JWT-Auth package)
+    Route::group(['middleware' => 'auth:api'], function() { // Protect routes (by requiring authentication to access them) that require authentication in our application: Apply Laravel's built-in 'auth' Middleware which requires the user to be authenticated to access the route/s (N.B. 'auth:api' tells Laravel to use the 'api' guard (not 'web') which is configured in 'config/auth.php' file to use the 'jwt' 'driver' of the Tymon JWT-Auth package). This way, we don't need to use    Auth::shouldUse('api'); (especially before request reaches Policy classes),nor auth('api'), nor Auth::guard('api')    within the controller methods of these routes.
         Route::apiResource('posts'         , PostAPIController::class); // API Resource Controller (excludes 'create' & 'edit' methods automatically)
         Route::apiResource('posts.comments', CommentAPIController::class)->shallow(); // Shallow Nested API Resource Controller (Post has many Comments)
 
@@ -50,7 +51,7 @@ Route::prefix('v1')->group(function() {
 
 
 
-// Similar working route setup (Note: Authentication Routes doesn't have the '/auth' prefix here with this setup)
+// Similar working route setup (Note: In this route setup, Authentication Routes doesn't have neither the 'api' Middleware Group nor the '/auth' prefix, i.e., the Route::group() has been ommitted altogether)
 /*
     Route::prefix('v1')->group(function() {
         // Authentication Routes (i.e., getting authenticated)  (Tymon JWT-Auth package authentication)
@@ -66,7 +67,7 @@ Route::prefix('v1')->group(function() {
 
 
         // Posts & Comments Routes (Protected Routes which require authentication (Tymon JWT-Auth package))
-        Route::group(['middleware' => 'auth:api'], function() {  // Protect routes (by requiring authentication to access them) that require authentication in our application: Apply Laravel's built-in 'auth' Middleware which requires the user to be authenticated to access the route/s (N.B. 'auth:api' tells Laravel to use the 'api' guard (not 'web') which is configured in 'config/auth.php' file to use the 'jwt' 'driver' of the Tymon JWT-Auth package)
+        Route::group(['middleware' => 'auth:api'], function() { // Protect routes (by requiring authentication to access them) that require authentication in our application: Apply Laravel's built-in 'auth' Middleware which requires the user to be authenticated to access the route/s (N.B. 'auth:api' tells Laravel to use the 'api' guard (not 'web') which is configured in 'config/auth.php' file to use the 'jwt' 'driver' of the Tymon JWT-Auth package). This way, we don't need to use    Auth::shouldUse('api'); (especially before request reaches Policy classes),nor auth('api'), nor Auth::guard('api')    within the controller methods of these routes.
             Route::apiResource('posts'         , PostAPIController::class); // API Resource Controller (excludes 'create' & 'edit' methods automatically)
             Route::apiResource('posts.comments', CommentAPIController::class)->shallow(); // Shallow Nested API Resource Controller (Post has many Comments)
 
---
+++ tests/Unit/APIAuthenticationControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+// HTTP Tests: https://laravel.com/docs/11.x/http-tests
+// Created a .env.testing file to use a separate SQLite "in-memory" database for testing (switched to using the main .env file (using the main database) and the DatabaseTransactions trait to rollback changes in the database after each test to not affect data in the main database)
+// Run php artisan migrate --env=testing to migrate the SQLite defined in .env.testing file for testing
+
+namespace Tests\Unit;
+
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase; // We have to import this class as instructed in Laravel 11 documentation: https://laravel.com/docs/11.x/http-tests
+// use Illuminate\Foundation\Testing\RefreshDatabase; // This trait also migrate database automatically    // Resetting the Database After Each Test: https://laravel.com/docs/11.x/database-testing#resetting-the-database-after-each-test
+use Illuminate\Foundation\Testing\DatabaseTransactions; // This trait automatically rollbacks database after each test (to not affect data in our existing database)    // Resetting the Database After Each Test: https://laravel.com/docs/11.x/database-testing#resetting-the-database-after-each-test
+
+class APIAuthenticationControllerTest extends TestCase
+{
+    // use RefreshDatabase; // This trait also migrate database automatically    // This trait will automatically migrate the database before each test and refresh (empty data) the database after each test    // Resetting the Database After Each Test: https://laravel.com/docs/11.x/database-testing#resetting-the-database-after-each-test
+    use DatabaseTransactions; // This trait automatically rollbacks database after each test (to not affect data in our existing database)    // Resetting the Database After Each Test: https://laravel.com/docs/11.x/database-testing#resetting-the-database-after-each-test
+
+    /**
+     * A basic unit test example.
+     */
+    // public function test_example(): void
+    // {
+    //     $this->assertTrue(true);
+    // }
+
+
+
+    public function test_user_can_register() {
+        $postDataToRegisterAPI = [
+            'name'     => 'Test UserTest',
+            'email'    => 'my.test@Test.com', // Make sure to use an email which doesn't already exist in the main database
+            'password' => '123456'
+        ];
+
+        $registerAPIResponse = $this->post('/api/v1/auth/register', $postDataToRegisterAPI); // Make a POST request to this route with the user data to register
+
+        $registerAPIResponse->assertStatus(201); // Assert that the Register API response status is 201 (i.e., Created)
+    }
+
+}
